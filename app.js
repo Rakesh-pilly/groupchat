34af6b4f67fca02c19741c4b24e24fa87bc57895
rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2022-08-17 15:02:09 +0530

    added for the

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const path = require('path');
 
 
 const app  = express();
 
 const PROT = process.env.PROT || 5000
 
+
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.listen(PROT, ()=> {
     console.log("this app is listting to ", PROT)
